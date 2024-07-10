a7a513a8e611c86023d600b3f612cc63cf8715d8
dinosaw44 <173938953+dinosaw44@users.noreply.github.com>
2024-07-10 07:37:07 +0100

    add list components

---
+++ src/lib/ui/Card/TaskCard.svelte
@@ -0,0 +1,42 @@
+<script lang=ts>
+    export let title: string
+</script>
+
+<style lang=scss>
+    button {
+        display: contents;
+        text-align: inherit;
+    }
+
+    article {
+        background-color: var(--color-light);
+        border: solid 2px transparent;
+        border-radius: .25rem;
+        color: var(--color-dark);
+        padding: .5rem 1rem;
+
+        &:hover {
+            background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.1), transparent);
+            border-color: var(--color-accent);
+        }
+    }
+
+    h1 {
+        font-size: inherit;
+        font-weight: inherit;
+        text-transform: lowercase;
+
+        &::first-letter {
+            text-transform: uppercase;
+        }
+    }
+</style>
+
+<button type=button on:click>
+    <article style:display=flex>
+        <hgroup style:display=contents>
+            <h1 style:flex=auto style:margin=auto>{title}</h1>
+        </hgroup>
+        <slot />
+    </article>
+</button>
--- src/routes/(client)/+page.svelte
+++ src/routes/(client)/+page.svelte
@@ -1,11 +1,42 @@
-<script lang=ts context=module>
-    const COUNT_KEY = "count"
-</script>
-
 <script lang=ts>
-    let count = Number(localStorage.getItem(COUNT_KEY)) ?? 0
-    $: localStorage.setItem(COUNT_KEY, String(count))
+    import TaskCard from "$lib/ui/Card/TaskCard.svelte"
+
+    const data = [
+        { title: "this is a test item" },
+        { title: "this is a test item" },
+        { title: "this is a test item", complete: true },
+        { title: "this is a test item" },
+    ]
 </script>
 
-<button on:click={() => count++}>+</button>
-<span>{count}</span>
+<style lang=scss>
+    main {
+        padding: 2rem;
+    }
+
+    .stack {
+        display: grid;
+        gap: .5rem;
+    }
+
+    li:has(input:checked) :global(h1) {
+        text-decoration: line-through;
+    }
+</style>
+
+<main class=stack>
+    <div>
+        <input />
+        <button>+ Add</button>
+    </div>
+
+    <ul class=stack>
+        {#each data as { title, complete }}
+            <li style:user-select=none>
+                <TaskCard {title} on:click={() => complete = !complete}>
+                    <input type=checkbox checked={complete} />
+                </TaskCard>
+            </li>
+        {/each}
+    </ul>
+</main>
